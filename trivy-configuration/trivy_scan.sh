dc3da335fd88f1bc5593316d2b8d16bcf783002a
Yogesh <yogesh.b@thoughtworks.com>
2022-09-30 14:14:21 +0530

    Yogesh | Added a shell script that scans for secrets and vulnerabilities in libraries

---
+++ trivy-configuration/trivy_scan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+TARGET_PATHS=("$@")
+if [ -z "$TARGET_PATHS" ]; then
+    TARGET_PATHS=(".")
+fi
+for TARGET_PATH in "${TARGET_PATHS[@]}";
+do
+echo "Started Scanning $TARGET_PATH"
+trivy fs --exit-code 1 "$TARGET_PATH"
+echo "Completed Scanning $TARGET_PATH"
+done
