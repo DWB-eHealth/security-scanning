9f6b960aa006901d30886bd6befb560a08e152b9
Yogesh <yogesh.b@thoughtworks.com>
2022-10-11 09:48:50 +0530

    Yogesh | Added debug mode in trivy command

--- security-scanning-configuration/security_scan.sh
+++ security-scanning-configuration/security_scan.sh
@@ -7,6 +7,6 @@ for TARGET_PATH in "${TARGET_PATHS[@]}";
 do
 echo "Started Scanning $TARGET_PATH"
 echo $TARGET_PATH
-trivy fs "$TARGET_PATH"
+trivy --no-progress --exit-code 1 --severity HIGH,CRITICAL --auto-refresh "$TARGET_PATH"
 echo "Completed Scanning $TARGET_PATH"
 done
