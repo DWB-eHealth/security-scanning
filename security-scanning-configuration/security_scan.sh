af8202c323355a5d91f9ea42d4b12a5a7ba8ba82
Yogesh <yogesh.b@thoughtworks.com>
2022-10-14 16:12:28 +0530

    Yogesh, Venky, Amrita | Added trivy installation steps

--- security-scanning-configuration/security_scan.sh
+++ security-scanning-configuration/security_scan.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+
+#Install Trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy
+
+#Add . to the target path if there are no target paths specified
 TARGET_PATHS=("$@")
 if [ -z "$TARGET_PATHS" ]; then
     TARGET_PATHS=(".")
 fi
+
+#Scan for secrets in target path
 for TARGET_PATH in "${TARGET_PATHS[@]}";
 do
 echo "Started Scanning $TARGET_PATH"
-echo $TARGET_PATH
+if test -f "$TARGET_PATH/trivy-secret.yaml" ;
+then
+trivy --exit-code 1 fs --security-checks secret "$TARGET_PATH" --secret-config  "$TARGET_PATH/trivy-secret.yaml"
+else
 trivy --exit-code 1 fs --security-checks secret "$TARGET_PATH"
+fi
 echo "Completed Scanning $TARGET_PATH"
 done
